312ef6b2b8276b747616e5784fe8bcf0d7aa4744
maticek1234 <119119858+maticek1234@users.noreply.github.com>
2026-02-26 10:54:00 +0100

    Update inmp441_db.h

--- components/inmp441_db/inmp441_db.h
+++ components/inmp441_db/inmp441_db.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "esphome.h"
+#include "driver/i2s.h"
 
+namespace esphome {
+namespace inmp441_db {
+
+class INMP441DB : public Component, public sensor::Sensor {
+ public:
+  void set_pins(GPIOPin *lrclk, GPIOPin *bclk, GPIOPin *din);
+  void setup() override;
+  void loop() override;
+
+ protected:
+  GPIOPin *lrclk_pin_;
+  GPIOPin *bclk_pin_;
+  GPIOPin *din_pin_;
+
+  float calculate_db(int32_t sample);
+};
+
+}  // namespace inmp441_db
+}  // namespace esphome
