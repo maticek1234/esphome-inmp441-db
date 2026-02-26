619c22b8f9645fa1499ec4a8dcd41a7a01d851e4
maticek1234 <119119858+maticek1234@users.noreply.github.com>
2026-02-26 10:44:14 +0100

    Update inmp441_db.cpp

--- components/inmp441_db/inmp441_db.cpp
+++ components/inmp441_db/inmp441_db.cpp
@@ -1,1 +1,18 @@
+#include "inmp441_db.h"
+#include "esphome/core/log.h"
 
+namespace esphome {
+namespace inmp441_db {
+
+static const char *TAG = "inmp441_db";
+
+void INMP441DB::setup() {
+  ESP_LOGI(TAG, "INMP441DB sensor setup complete");
+}
+
+void INMP441DB::dump_config() {
+  ESP_LOGCONFIG(TAG, "INMP441DB Sensor");
+}
+
+}  // namespace inmp441_db
+}  // namespace esphome
