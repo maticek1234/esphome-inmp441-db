f32caeb8e8c9dd69758b9b374800849279416214
maticek1234 <119119858+maticek1234@users.noreply.github.com>
2026-02-26 10:53:30 +0100

    Update sensor.py

--- components/inmp441_db/sensor.py
+++ components/inmp441_db/sensor.py
@@ -1,22 +1,38 @@
-
-"""ESPHome INMP441 DB Sensor Component"""
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME
+from esphome import pins
 
-CODEOWNERS = ["@maticek1234"]
+DEPENDENCIES = ["esp32"]
 
-inmp441_db_ns = cg.esphome_ns.namespace("inmp441_db")
-INMP441DB = inmp441_db_ns.class_("INMP441DB", sensor.Sensor, cg.Component)
+CONF_LRCLK = "lrclk_pin"
+CONF_BCLK = "bclk_pin"
+CONF_DIN = "din_pin"
 
-CONFIG_SCHEMA = sensor.sensor_schema(INMP441DB).extend(
-    {
-        cv.GenerateID(): cv.declare_id(INMP441DB),
-    }
+inmp441_ns = cg.esphome_ns.namespace("inmp441_db")
+INMP441DB = inmp441_ns.class_("INMP441DB", cg.Component, sensor.Sensor)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema()
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(INMP441DB),
+            cv.Required(CONF_LRCLK): pins.gpio_output_pin_schema,
+            cv.Required(CONF_BCLK): pins.gpio_output_pin_schema,
+            cv.Required(CONF_DIN): pins.gpio_input_pin_schema,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+
+    lrclk = await cg.gpio_pin_expression(config[CONF_LRCLK])
+    bclk = await cg.gpio_pin_expression(config[CONF_BCLK])
+    din = await cg.gpio_pin_expression(config[CONF_DIN])
+
+    cg.add(var.set_pins(lrclk, bclk, din))
